ec3fe19b87f48e57ae8b2e47f105758bfe5db440
Phong Tran <trandiphong004@gmail.com>
2025-04-09 21:30:49 +0700

    6. React Router

--- lab07/22717861_TranDiPhong_ThucHanhPTGDUD_lab07/src/App.jsx
+++ lab07/22717861_TranDiPhong_ThucHanhPTGDUD_lab07/src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import Teams from './pages/Teams';
+import Analytics from './pages/Analytics';
+import Messages from './pages/Messages';
+import Integrations from './pages/Integrations';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -18,6 +23,11 @@ function App() {
         {/* Sử dụng React Router, Chia ra thành các page, gom thành SPA */}
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/integrations" element={<Integrations />} />
         </Routes>
       </div>
     </div>
